641b40a88953077a5b510fa1559c6e7ebcbe4a65
Nereid97 <poltavskaya.dasha@mail.ru>
2017-11-28 21:02:57 +0200

    Change sin to [ etc

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Windows.Forms;
 using KursProject1.DifferentiationStrategies;
 using KursProject1.SimplifyStrategies;
+using System.Text.RegularExpressions;
 
 namespace KursProject1
 {
@@ -17,7 +18,14 @@ namespace KursProject1
         private void button1_Click(object sender, EventArgs e)
         {
             var s = textBox1.Text.Replace(" ", String.Empty);
-            
+            s = Regex.Replace(s, "[s][i][n]", "[", RegexOptions.IgnoreCase);
+            s = Regex.Replace(s, "[c][o][s]", "]", RegexOptions.IgnoreCase);
+            s = Regex.Replace(s, "[t][a][n]", "!", RegexOptions.IgnoreCase);
+            s = Regex.Replace(s, "[l][n]", "?", RegexOptions.IgnoreCase);
+            s = Regex.Replace(s, "[e][x][p]", "#", RegexOptions.IgnoreCase);
+            s = Regex.Replace(s, "[s][q][r][t]", "%", RegexOptions.IgnoreCase);
+            var changedS = textBox1.Text.Replace(" ", String.Empty);
+
             Tree = new Tree(s, new NodeTypeDeterminator());
 
             Tree.ProcessTree(new Context.Context(new DifferentiationStrategiesSetter()));
@@ -26,8 +34,14 @@ namespace KursProject1
             {
                 Tree.ProcessTree(new Context.SimplifyContext(new SimplifyStrategiesSetter(), chkSimpliestForm?.Checked ?? false));
             }
-
-            textBox2.Text = Tree.Head.Output();
+            string result = Tree.Head.Output();
+            result = Regex.Replace(result, "[[]", "sin");
+            result = Regex.Replace(result, "[]]", "cos");
+            result = Regex.Replace(result, "[!]", "tan");
+            result = Regex.Replace(result, "[?]", "ln");
+            result = Regex.Replace(result, "[#]", "exp");
+            result = Regex.Replace(result, "[%]", "sqrt");
+            textBox2.Text = result;
         }
 
         private void chkbtnSimplify_CheckedChanged(object sender, EventArgs e)
@@ -39,7 +53,14 @@ namespace KursProject1
         private void btnSimplify_Click(object sender, EventArgs e)
         {
             Tree?.ProcessTree(new Context.SimplifyContext(new SimplifyStrategiesSetter(), chkSimpliestForm?.Checked ?? false));
-            textBox2.Text = Tree?.Head.Output();
+            string result = Tree.Head.Output();
+            result = Regex.Replace(result, "[[]", "sin");
+            result = Regex.Replace(result, "[]]", "cos");
+            result = Regex.Replace(result, "[!]", "tan");
+            result = Regex.Replace(result, "[?]", "ln");
+            result = Regex.Replace(result, "[#]", "exp");
+            result = Regex.Replace(result, "[%]", "sqrt");
+            textBox2.Text = result;
         }
     }
 }
