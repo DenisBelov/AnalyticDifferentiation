e80a643fd69daa628e13d67c74bb5649ae8d9ed1
Nereid97 <poltavskaya.dasha@mail.ru>
2017-12-02 21:22:35 +0200

    little error

--- Form1.cs
+++ Form1.cs
@@ -15,57 +15,83 @@ namespace KursProject1 {
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //try {
             var s = textBox1.Text.Replace(" ", String.Empty);
-            s = Regex.Replace(s, "[s][i][n]", "[", RegexOptions.IgnoreCase);
-            s = Regex.Replace(s, "[c][o][s]", "]", RegexOptions.IgnoreCase);
-            s = Regex.Replace(s, "[t][a][n]", "!", RegexOptions.IgnoreCase);
-            s = Regex.Replace(s, "[l][n]", "?", RegexOptions.IgnoreCase);
-            s = Regex.Replace(s, "[e][x][p]", "#", RegexOptions.IgnoreCase);
-            s = Regex.Replace(s, "[s][q][r][t]", "%", RegexOptions.IgnoreCase);
+                s = Regex.Replace(s, "[s][i][n]", "[", RegexOptions.IgnoreCase);
+                s = Regex.Replace(s, "[c][o][s]", "]", RegexOptions.IgnoreCase);
+                s = Regex.Replace(s, "[t][a][n]", "!", RegexOptions.IgnoreCase);
+                s = Regex.Replace(s, "[l][n]", "?", RegexOptions.IgnoreCase);
+                s = Regex.Replace(s, "[e][x][p]", "#", RegexOptions.IgnoreCase);
+                s = Regex.Replace(s, "[s][q][r][t]", "%", RegexOptions.IgnoreCase);
 
 
-            Tree = new Tree(s, new NodeTypeDeterminator());
+                Tree = new Tree(s, new NodeTypeDeterminator());
 
-            Tree.ProcessTree(new Context.Context(new DifferentiationStrategiesSetter()));
+                Tree.ProcessTree(new Context.Context(new DifferentiationStrategiesSetter()));
 
-            if (chkSimplify?.Checked ?? false)
-            {
-                Tree.ProcessTree(new Context.SimplifyContext(new SimplifyStrategiesSetter(), chkSimpliestForm?.Checked ?? false));
-            }
-            string result = Tree.Head.Output();
-            result = Regex.Replace(result, "[[]", "sin");
-            result = Regex.Replace(result, "[]]", "cos");
-            result = Regex.Replace(result, "[!]", "tan");
-            result = Regex.Replace(result, "[?]", "ln");
-            result = Regex.Replace(result, "[#]", "exp");
-            result = Regex.Replace(result, "[%]", "sqrt");
-            textBox2.Text = result;
+                if (chkSimplify?.Checked ?? false)
+                {
+                    Tree.ProcessTree(new Context.SimplifyContext(new SimplifyStrategiesSetter(), chkSimpliestForm?.Checked ?? false));
+                }
+                string result = Tree.Head.Output();
+                result = Regex.Replace(result, "[[]", "sin");
+                result = Regex.Replace(result, "[]]", "cos");
+                result = Regex.Replace(result, "[!]", "tan");
+                result = Regex.Replace(result, "[?]", "ln");
+                result = Regex.Replace(result, "[#]", "exp");
+                result = Regex.Replace(result, "[%]", "sqrt");
+                textBox2.Text = result;
+            //}
+            //catch (Exception exc)
+            //{
+            //    MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            //}
         }
 
         private void chkbtnSimplify_CheckedChanged(object sender, EventArgs e)
         {
-            var chkbtn = sender as CheckBox;
-            btnSimplify.Visible = !chkbtn?.Checked ?? true;
+            try
+            {
+                var chkbtn = sender as CheckBox;
+                btnSimplify.Visible = !chkbtn?.Checked ?? true;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         private void btnSimplify_Click(object sender, EventArgs e)
         {
-            Tree?.ProcessTree(new Context.SimplifyContext(new SimplifyStrategiesSetter(), chkSimpliestForm?.Checked ?? false));
-            string result = Tree.Head.Output();
-            result = Regex.Replace(result, "[[]", "sin");
-            result = Regex.Replace(result, "[]]", "cos");
-            result = Regex.Replace(result, "[!]", "tan");
-            result = Regex.Replace(result, "[?]", "ln");
-            result = Regex.Replace(result, "[#]", "exp");
-            result = Regex.Replace(result, "[%]", "sqrt");
-            textBox2.Text = result;
+            //try {
+                Tree?.ProcessTree(new Context.SimplifyContext(new SimplifyStrategiesSetter(), chkSimpliestForm?.Checked ?? false));
+                string result = Tree.Head.Output();
+                result = Regex.Replace(result, "[[]", "sin");
+                result = Regex.Replace(result, "[]]", "cos");
+                result = Regex.Replace(result, "[!]", "tan");
+                result = Regex.Replace(result, "[?]", "ln");
+                result = Regex.Replace(result, "[#]", "exp");
+                result = Regex.Replace(result, "[%]", "sqrt");
+                textBox2.Text = result;
+           // }
+            //catch (Exception exc)
+            //{
+            //    MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            //}
         }
 
         private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            try
             {
-                textBox1.Text = FileReader.GetExpressionFromFile(openFileDialog1.FileName);
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = FileReader.GetExpressionFromFile(openFileDialog1.FileName);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
             
         }
